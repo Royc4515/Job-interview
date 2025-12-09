93fdb86fc7bb9cefb58ef1f7c11a733135d30b7f
Royc4515 <Roy.y.carmelli@gmail.com>
2025-12-09 22:28:07 +0200

    Add assembly program to print question and answer first question

---
+++ q1.asm
@@ -0,0 +1,30 @@
+File: q1.asm
+
+section .data
+    q  db "באיזה צבע השמש ? ",10
+    q_len equ $-q
+
+    a  db "אדום",10
+    a_len equ $-a
+
+section .text
+    global _start
+
+_start:
+    ; print question
+    mov rax,1
+    mov rdi,1
+    mov rsi,q
+    mov rdx,q_len
+    syscall
+
+    ; print (wrong) answer
+    mov rax,1
+    mov rdi,1
+    mov rsi,a
+    mov rdx,a_len
+    syscall
+
+    mov rax,60
+    xor rdi,rdi
+    syscall
